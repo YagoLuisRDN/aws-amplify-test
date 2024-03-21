48bed2254a54426fa2a158082664d9c7102a454c
YagoLuisRDN <yago.luis@redesignconsultoria.com.br>
2024-03-21 16:55:13 -0300

    feat: add form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {
+  NewForm1 
+ } from './ui-components';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +29,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <NewForm1 />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>

73efb635dd5bd339f0f145861312343c59601a56
YagoLuisRDN <yago.luis@redesignconsultoria.com.br>
2024-03-21 11:21:06 -0300

    feat: add title in count

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,12 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
+          <p>Contador:</p>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <p>CI/CD</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
